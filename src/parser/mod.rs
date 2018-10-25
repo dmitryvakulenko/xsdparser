c72e873513851ac8678081e53064041a5b74b49e
Dmitry Vakulenko <n0dwis@gmail.com>
2018-10-25 19:04:11 +0300

    One more fix

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,7 +5,8 @@ mod model;
 use std::fs::File;
 use std::io::BufReader;
 use std::string::String;
-use parser::xml::reader::{EventReader, XmlEvent};
+use self::xml::reader::{EventReader, XmlEvent};
+use std::rc::Rc;
 
 pub fn build_tree(file_name: &String) -> model::Node {
     let file = File::open(file_name).unwrap();
@@ -13,25 +14,22 @@ pub fn build_tree(file_name: &String) -> model::Node {
 
     let parser = EventReader::new(file);
     let mut root = model::Node::new();
-    let mut nodes = Vec::<&mut model::Node>::new();
+    let mut nodes = Vec::<Rc<&mut model::Node>>::new();
     for e in parser {
         match e {
             c @ Ok(XmlEvent::StartElement { .. }) => {
                 let mut new_node = model::Node::new();
-                let add_node: &mut model::Node;
-                {
-                    add_node = match nodes.last_mut() {
-                        Some(ref mut n) =>
-                            n.add_child(new_node),
-                        None =>
-                            root.add_child(new_node)
-                    };
-                }
-                nodes.push(add_node)
-            },
+                let added_node = match nodes.last_mut() {
+                    Some(n) =>
+                        n.add_child(new_node),
+                    None =>
+                        root.add_child(new_node)
+                };
+                nodes.push(Rc::new(added_node))
+            }
             Ok(XmlEvent::EndElement { .. }) => {
                 nodes.pop();
-            },
+            }
             Err(e) => {
                 println!("Error: {}", e);
                 break;
